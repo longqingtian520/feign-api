e461982da3193c9e97921bb178c9df33c1473d98
wangqiubao <wangqiubao@u-care.net>
2019-02-14 16:37:01 +0800

    feign接口

---
+++ src/main/java/com/criss/wang/FeginApiApplication.java
@@ -0,0 +1,16 @@
+package com.criss.wang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients({"com.criss.wang.api.**"})
+@SpringBootApplication
+public class FeginApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeginApiApplication.class, args);
+	}
+
+}
+
---
+++ src/main/java/com/criss/wang/api/FeignApi.java
@@ -0,0 +1,13 @@
+package com.criss.wang.api;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient("criss-feign")
+public interface FeignApi {
+
+	@RequestMapping(value = "/fegin", method = RequestMethod.GET)
+	public String feginTest(@RequestParam(value = "name", required = false) String name);
+}
